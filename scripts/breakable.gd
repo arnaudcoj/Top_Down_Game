91dc7d9fdde583581c9804cd87dfa284267e747f
Kaendan <tristan.camus@gmail.com>
2016-06-28 03:26:23 +0200

    Created a singleton sound_player Added a sound when we cut a bush

--- scripts/breakable.gd
+++ scripts/breakable.gd
@@ -3,8 +3,9 @@ extends Sprite
 var sword_hit = preload("res://scripts/sword_hit.gd")
 
 func _ready():
-	get_node("Area2D").connect("area_enter", self, "_on_enter")
+	get_node("HitBox").connect("area_enter", self, "_on_enter")
 
 func _on_enter(area):
 	if (area extends sword_hit):
+		sound_player.play("cut_bush")
 		queue_free()
