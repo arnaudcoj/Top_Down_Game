9adfb08884923268c88cb87bb190a12a4446214b
Kaendan <tristan.camus@gmail.com>
2016-06-29 14:49:01 +0200

    Added SamplePlayer2D to bushes, solids and bats

--- scripts/breakable.gd
+++ scripts/breakable.gd
@@ -9,6 +9,7 @@ func _ready():
 func _on_enter(area):
 	if (area extends sword_hit):
 		get_node("Sprite").queue_free()
+		get_node("HitBox").queue_free()
 		get_node("SoundPlayer").play("cut_bush")
 		get_node("Particles").set_emitting(true)
 		get_node("Timer").start()
--- scripts/sword_hit.gd
+++ scripts/sword_hit.gd
@@ -1,11 +1,17 @@
 extends Area2D
 
+var first = true
+
 func _ready():
-	if not controler.debug : hide()
 	get_node("Timer").connect("timeout", self, "_on_timeout")
 	get_node("Timer").start()
-	controler.soundPlayer.play("sword")
 	
-
 func _on_timeout():
-	queue_free()
+	if(first):
+		first = false
+		get_node("HitBox").queue_free()
+		get_node("SoundPlayer").play("sword")
+		get_node("Timer").set_wait_time(0.5)
+		get_node("Timer").start()
+	else: 
+		queue_free()
