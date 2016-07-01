2d14f580715d2e7edc2a56621ca775d5e743d9d9
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2016-07-01 15:14:37 +0200

    improve the textbox's auto-wrap feature from character wrapping to word wrapping

--- scripts/text_box.gd
+++ scripts/text_box.gd
@@ -7,6 +7,7 @@ onready var nb_rows = floor(text_label.get_size().height / text_label.get_font("
 
 var active = false
 var cursor = 0
+var visible_cursor = 0
 var paragraphs = []
 var current_paragraph = null
 
@@ -37,6 +38,8 @@ func next():
 			current_paragraph = paragraphs[0]
 			paragraphs.pop_front()
 			cursor = 0
+			visible_cursor = 0
+			text_label.set_visible_characters(visible_cursor)
 			text_label.clear()
 			timer.start()
 			return true
@@ -50,17 +53,33 @@ func add_paragraph(text):
 
 func add_character():
 	if current_paragraph :
+		if visible_cursor >= cursor && cursor < current_paragraph.length():
+			add_word()
+		visible_cursor += 1
+		text_label.set_visible_characters(visible_cursor)
+		
+func add_word():
+	# add the complete word, in order to get a nice auto wrapping
+	while cursor < current_paragraph.length() && current_paragraph[cursor] != ' ' :
 		text_label.add_text(current_paragraph[cursor])
 		cursor += 1
+	# add the last space
+	if cursor < current_paragraph.length() && current_paragraph[cursor] == ' ' :
+		text_label.add_text(current_paragraph[cursor])
+		cursor += 1
+
+		
 
 func clear():
 	if controler.debug : print("[textbox] clear")
 	paragraphs.clear()
 	current_paragraph = null
+	cursor = 0
+	visible_cursor = 0
 
 func _on_Timer_timeout():
 	if current_paragraph :
-		if cursor < current_paragraph.length() :
+		if visible_cursor < current_paragraph.length() :
 			add_character()
 			timer.start()
 		else :
